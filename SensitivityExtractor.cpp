15f5d44d4cff23e2699c5b7c7f5fe04eaccc8dc7
Darshana Adikari <darshanaads@gmail.com>
2018-09-15 20:17:12 +0200

    Auto-distribute file sizes.

--- SensitivityExtractor.cpp
+++ SensitivityExtractor.cpp
@@ -34,7 +34,7 @@
 const unsigned int MAX_THREADS = std::thread::hardware_concurrency();
 
 void sensitivityTesterFunc(const ObservationModelConfig cfg, std::istream &inputStream, std::ostream &outputStream,
-                           std::atomic<size_t> &iterations)
+                           std::atomic<size_t> &iterations, std::atomic<size_t> &sensitivityCount)
 {
     auto obs = ObservationSensitivityProvider::getSensitivityProvider(cfg);
 
@@ -68,6 +68,7 @@ void sensitivityTesterFunc(const ObservationModelConfig cfg, std::istream &input
                 }
             }
             outputStream << std::endl;
+            sensitivityCount++;
         }
     }
 }
@@ -115,6 +116,7 @@ int main(int argc, char **argv)
     {
         std::vector<std::thread> threadList(usableThreads);
         std::vector<std::atomic<size_t>> iterCount(usableThreads);
+        std::vector<std::atomic<size_t>> sensitivityCount(usableThreads);
         std::vector<std::fstream> outputFileList(usableThreads);
 
         const ObservationModelConfig cfg = {imSize, fc, cc, fov, 1000, maxGridPointsPerSide, 0.08};
@@ -124,7 +126,7 @@ int main(int argc, char **argv)
         // bool resumeFlags[THREADS_USED];
         for (unsigned int i = 0; i < usableThreads; i++)
         {
-            outputFileList[i] = std::fstream((outFileName + "_" + std::to_string(i) + ".txt"), std::ios::out);
+            outputFileList[i] = std::fstream((outFileName + "Temp_" + std::to_string(i) + ".txt"), std::ios::out);
             if (!outputFileList[i].is_open())
             {
                 std::cerr << "output file creation failed. Aborting!!!" << std::endl;
@@ -132,7 +134,7 @@ int main(int argc, char **argv)
             }
 
             threadList[i] = std::thread(sensitivityTesterFunc, cfg, std::ref(inputPoseAndJointStreams[i]),
-                                        std::ref(outputFileList[i]), std::ref(iterCount[i]));
+                                        std::ref(outputFileList[i]), std::ref(iterCount[i]), std::ref(sensitivityCount[i]));
         }
 
         /// Progress display
@@ -189,6 +191,53 @@ int main(int argc, char **argv)
         {
             outputFileList[i].close();
         }
+
+        std::cout << "Redistributing data" << std::endl;
+        // redistribute the written lines.
+        {
+            const size_t linesPerFile = std::accumulate(sensitivityCount.begin(),
+                                                        sensitivityCount.end(), (size_t)0) /
+                                        (size_t)usableThreads;
+            size_t outFileNum = 0;
+            std::fstream inStream;
+            std::fstream outStream = std::fstream((outFileName + "_" + std::to_string(outFileNum) + ".txt"), std::ios::out);
+            std::cout << "Write to file: " << outFileNum << std::endl;
+            size_t counter = 0;
+            size_t totalCounter = 0;
+
+            for (size_t i = 0; i < usableThreads; i++)
+            {
+                std::cout << "Read file: " << i << std::endl;
+                inStream.open((outFileName + "Temp_" + std::to_string(i) + ".txt"), std::ios::in);
+                if (!inStream)
+                {
+                    continue;
+                }
+                std::string s;
+                while (std::getline(inStream, s))
+                {
+                    outStream << s << std::endl;
+                    counter++;
+                    totalCounter++;
+                    if (counter > linesPerFile && (outFileNum + 1) < usableThreads) // if last file, keep appending to the same file.
+                    {
+                        outStream.close();
+                        outStream.clear();
+                        std::cout << "Finished writing to file: " << outFileNum << " written: " << counter << std::endl;
+                        counter = 0;
+                        outFileNum++;
+                        std::cout << "Open write to file: " << outFileNum << std::endl;
+                        outStream.open((outFileName + "_" + std::to_string(outFileNum) + ".txt"), std::ios::out);
+                    }
+                }
+                inStream.close();
+                inStream.clear();
+                // deleting the temp file.
+                std::remove((outFileName + "Temp_" + std::to_string(i) + ".txt").c_str());
+            }
+            std::cout << "Total redistributed lines: " << totalCounter << std::endl;
+        }
+
 #endif
     }
 
