e8f770f47853308a0661d8d83c48d5494d72e45c
Darshana Adikari <darshanaads@gmail.com>
2019-01-08 00:28:33 +0100

    Disable interactionTest for now

--- src/tests/interactiontest.cpp
+++ src/tests/interactiontest.cpp
@@ -1,9 +1,9 @@
-#include <algorithm>
-#include <assert.h>
-#include <cmath>
-#include <fstream>
-#include <iostream>
-#include <limits>
-#include <numeric>
-#include <thread>
+//#include <algorithm>
+//#include <assert.h>
+//#include <cmath>
+//#include <fstream>
+//#include <iostream>
+//#include <limits>
+//#include <numeric>
+//#include <thread>
 
