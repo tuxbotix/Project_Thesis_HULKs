0d3401afa50e949fd08a21fb1fab368066c916cb
Darshana Adikari <darshanaads@gmail.com>
2019-01-08 02:24:18 +0100

    Fix interactionTest and renamed Interactions.hpp

--- src/lib/inc/interactions.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef INTERACTIONS_HPP
-#define INTERACTIONS_HPP
-
-#endif // INTERACTIONS_HPP
--- src/tests/interactiontest.cpp
+++ src/tests/interactiontest.cpp
@@ -1,9 +1,32 @@
-//#include <algorithm>
-//#include <assert.h>
-//#include <cmath>
-//#include <fstream>
-//#include <iostream>
-//#include <limits>
-//#include <numeric>
-//#include <thread>
+#include <algorithm>
+#include <assert.h>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <thread>
 
+#include "Interactions.hpp"
+#include "utils.hpp"
+
+using namespace Interactions;
+
+int main() {
+
+  for (size_t i = 1; i < 20; ++i) {
+    std::cout << i << "\t";
+    for (size_t j = 0; j < i; ++j) {
+      std::cout << "* ";
+
+      size_t idx = encodeInteractionVecIndex(i, j);
+      size_t row = 0, col = 0;
+      decodeInteractionVecIndex(idx, row, col);
+      if (idx != encodeInteractionVecIndex(row, col)) {
+        std::cout << "SOMETHING IS WRONG!!!" << std::endl;
+      }
+    }
+    std::cout << "\n";
+  }
+  std::cout << std::endl;
+}
