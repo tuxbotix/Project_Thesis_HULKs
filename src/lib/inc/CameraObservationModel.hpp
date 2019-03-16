0ca5c18e5188ab168ffd8fdebfdbc3f41dc87e7b
Darshana Adikari <darshanaads@gmail.com>
2019-03-16 22:04:59 +0100

    Some cleanup in cameraObservationModel

--- src/lib/inc/CameraObservationModel.hpp
+++ src/lib/inc/CameraObservationModel.hpp
@@ -22,29 +22,16 @@ private:
  */
   typedef int16_t sensitivityOutputType;
   static const std::string name;
-  //  static constexpr float maxViewDist = 3.0;
-  // we scale the output to a signed integer occupying the below byte count.
-  // ie: to fit
-  // static const uint8_t bytesPerDim = sizeof(sensitivityOutputType);
-  const uint64_t
-      maxValPerDim; // = std::numeric_limits<sensitivityOutputType>::max(); //
-                    // max per side*, so - 38768
 
-  // const size_t maxGridPointsPerSide;
-
+  const uint64_t maxValPerDim;
   Vector2i imSize;
-  // float gridSpacing;
   const float deltaThetaCorse;
   const float deltaThetaFine;
 
-  // const Vector3f dimensionMax;
-
-  // TODO make this private??
-public:
   NaoJointAndSensorModel naoJointSensorModel;
 
 public:
-  ~CameraObservationModel() {}
+  virtual ~CameraObservationModel() {}
   // friend class ObservationSensitivity;
   CameraObservationModel(const Vector2i &imSize, const Vector2f &fc,
                          const Vector2f &cc, const Vector2f &fov,
@@ -59,14 +46,6 @@ public:
    */
   void updateState(const rawPoseT &jointAngles, const SUPPORT_FOOT &sf,
                    const Camera &sensorNames);
-
-  //  /**
-  //   * Get 3D grid with given grid spacing relative to robot's ground coord.
-  //   * Obviously, camera matrix must be updated before calling this.
-  //   * 1. This has a hard range limit of 2m
-  //   */
-  //  // TODO  Make this private.
-  //  VecVector2<float> getGroundGrid(const Camera &camName, bool &success);
 
   /**
    * Get observability (sensitivity?) of a given joint for a given camera at a
